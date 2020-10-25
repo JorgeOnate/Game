d655b15055fffc8439e07473d363c6fb8555539b
JorgeOnate <jorge.onate.2017@upb.edu.co>
2020-10-25 15:02:18 -0500

    Adjustments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
